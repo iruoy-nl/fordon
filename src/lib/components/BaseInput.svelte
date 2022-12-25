1a77299dff0148a8a80a1daad03ee54c4decaf6c
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-12-25 18:10:53 +0100

    refactor: restyle the login page

---
+++ src/lib/components/BaseInput.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  /**
+   * The label to display above the field.
+   */
+  export let label: string = "";
+
+  /**
+   * A placeholder to display within the field.
+   */
+  export let placeholder: string = "";
+
+  /**
+   * The type of input to display.
+   */
+  export let type: "text" | "email" | "password" = "text";
+
+  /**
+   * The name of the value.
+   */
+  export let name: string;
+
+  /**
+   * The value of the field.
+   */
+  export let value: unknown;
+
+  /**
+   * An error message to display below the field.
+   */
+  export let error: string | null = null;
+</script>
+
+<label for={name} class="form-label">{label}</label>
+
+<div class="input-group has-validation">
+  <input
+    class="form-control"
+    class:is-invalid={error !== null}
+    required
+    {name}
+    {type}
+    {value}
+    {placeholder}
+  />
+
+  {#if error !== null}
+    <div class="invalid-feedback">
+      {error}
+    </div>
+  {/if}
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,77 +1,79 @@
 <script lang="ts">
+  import BaseInput from "$lib/components/BaseInput.svelte";
   import type { LoginForm } from "$lib/types";
 
   /**
-   * Form data on validation fail.
+   * Form data.
    */
   export let form: LoginForm & {
     errors: { [field: string]: string };
   };
 </script>
 
-<div class="row my-5 justify-content-center">
-  <div class="col-12 col-sm-8 col-lg-4">
-    <!-- Align everything within the 4 center columns -->
-    <form action="?/login" method="post">
-      <div class="row">
-        <div class="col">
-          <h2>Inloggen</h2>
-          <p>Vul je e-mailadres en wachtwoord in om naar Fordon te gaan.</p>
-        </div>
-
-        <div class="w-100" />
-
-        <!-- Email field -->
-        <div class="col mt-3">
-          <label for="email" class="form-label">E-mailadres</label>
-
-          <div class="input-group has-validation">
-            <input
-              class="form-control {form?.errors.email ? 'is-invalid' : ''}"
-              type="email"
-              name="email"
-              value={form?.email ?? null}
-              required
-            />
-
-            {#if form?.errors.email}
-              <div class="invalid-feedback">
-                {form?.errors.email[0]}
-              </div>
-            {/if}
-          </div>
-        </div>
-
-        <div class="w-100" />
-
-        <!-- Password field -->
-        <div class="col mt-3">
-          <label for="password" class="form-label"> Wachtwoord </label>
-
-          <div class="input-group has-validation">
-            <input
-              class="form-control {form?.errors.password ? 'is-invalid' : ''}"
-              type="password"
-              name="password"
-              value={form?.password ?? ""}
-              required
-            />
-
-            {#if form?.errors.password}
-              <div class="invalid-feedback">
-                {form?.errors.password[0]}
-              </div>
-            {/if}
-          </div>
-        </div>
-
-        <div class="w-100" />
-
-        <!-- Submit button -->
-        <div class="col mt-3">
-          <button class="btn btn-primary">Inloggen</button>
-        </div>
+<div class="row vh-100">
+  <div class="col-6">
+    <div class="row h-100 p-4">
+      <!-- Header -->
+      <div class="col-12">
+        <span class="text-primary">● Fordon</span>
       </div>
-    </form>
+
+      <!-- Main -->
+      <div class="col">
+        <form action="?/login" method="post">
+          <div class="row justify-content-center">
+            <div class="col-6">
+              <h2>Welcome back</h2>
+              <p class="text-muted">
+                Please enter your credentials to continue.
+              </p>
+            </div>
+
+            <div class="w-100 my-3" />
+
+            <div class="col-6">
+              <BaseInput
+                label="Email"
+                name="email"
+                placeholder="Enter your email"
+                value={form?.email || ""}
+                type="email"
+                error={form?.errors?.email[0]}
+              />
+            </div>
+
+            <div class="w-100 my-2" />
+
+            <div class="col-6">
+              <BaseInput
+                label="Password"
+                name="password"
+                placeholder="●●●●●●●●"
+                value={form?.password || ""}
+                type="password"
+                error={form?.errors?.password[0]}
+              />
+            </div>
+
+            <div class="w-100 my-3" />
+
+            <div class="col-6">
+              <button class="btn btn-primary">Login</button>
+            </div>
+
+            <div class="w-100 my-3" />
+
+            <div class="col-6 text-center">
+              <span class="text-muted">Don't have an account?</span>
+              <a href="/register" class="text-primary">Register</a>
+            </div>
+
+            <div class="w-100 my-5" />
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
+
+  <div class="col-6 bg-light" />
 </div>
