da74f7a146af665b53e095d4654387f8d2bb2b21
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-12-26 16:48:15 +0100

    refactor: add an image to the login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,70 +5,82 @@
   export let form: ActionData;
 </script>
 
-<div class="row vh-100">
-  <div class="col-6">
-    <div class="row h-100 p-4">
-      <!-- Header -->
-      <div class="col-12">
-        <span class="text-primary">● Fordon</span>
-      </div>
+<div class="container-fluid">
+  <div class="row vh-100">
+    <div class="col-6">
+      <div class="row h-100 p-4">
+        <!-- Header -->
+        <div class="col-12">
+          <span class="text-primary">● Fordon</span>
+        </div>
 
-      <!-- Main -->
-      <div class="col">
-        <form action="?/login" method="post">
-          <div class="row justify-content-center">
-            <div class="col-6">
-              <h2>Welcome back</h2>
-              <p class="text-muted">
-                Please enter your credentials to continue.
-              </p>
+        <!-- Main -->
+        <div class="col">
+          <form action="?/login" method="post">
+            <div class="row justify-content-center">
+              <div class="col-6">
+                <h2>Welkom terug</h2>
+                <p class="text-muted">
+                  Gebruik je e-mailadres en wachtwoord om door te gaan.
+                </p>
+              </div>
+
+              <div class="w-100 my-3" />
+
+              <div class="col-6">
+                <BaseInput
+                  label="E-mailadres"
+                  name="email"
+                  placeholder="Voer je e-mailadres in"
+                  value={form?.data.email || ""}
+                  type="email"
+                  error={form?.errors.fieldErrors.email?.at(0)}
+                />
+              </div>
+
+              <div class="w-100 my-2" />
+
+              <div class="col-6">
+                <BaseInput
+                  label="Wachtwoord"
+                  name="password"
+                  placeholder="●●●●●●●●"
+                  value=""
+                  type="password"
+                  error={form?.errors.fieldErrors.password?.at(0)}
+                />
+              </div>
+
+              <div class="w-100 my-3" />
+
+              <div class="col-6">
+                <button class="btn btn-primary">Inloggen</button>
+              </div>
+
+              <div class="w-100 my-3" />
+
+              <div class="col-6 text-center">
+                <span class="text-muted">Nog geen account?</span>
+                <a href="/register" class="text-primary">Registreren</a>
+              </div>
+
+              <div class="w-100 my-5" />
             </div>
-
-            <div class="w-100 my-3" />
-
-            <div class="col-6">
-              <BaseInput
-                label="Email"
-                name="email"
-                placeholder="Enter your email"
-                value={form?.data.email || ""}
-                type="email"
-                error={form?.errors.fieldErrors.email?.at(0)}
-              />
-            </div>
-
-            <div class="w-100 my-2" />
-
-            <div class="col-6">
-              <BaseInput
-                label="Password"
-                name="password"
-                placeholder="●●●●●●●●"
-                value=""
-                type="password"
-                error={form?.errors.fieldErrors.password?.at(0)}
-              />
-            </div>
-
-            <div class="w-100 my-3" />
-
-            <div class="col-6">
-              <button class="btn btn-primary">Login</button>
-            </div>
-
-            <div class="w-100 my-3" />
-
-            <div class="col-6 text-center">
-              <span class="text-muted">Don't have an account?</span>
-              <a href="/register" class="text-primary">Register</a>
-            </div>
-
-            <div class="w-100 my-5" />
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
 
-  <div class="col-6 bg-light" />
+    <div class="col-6 image" />
+  </div>
 </div>
+
+<style lang="scss">
+  .image {
+    background-image: url("https://images.unsplash.com/photo-1619677122678-b8e28f74cf42");
+    background-size: cover;
+
+    // Fallback
+    background-color: $light;
+  }
+</style>
