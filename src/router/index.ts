d1342dccad313dc11397b20d4730e2c94941a880
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-01-14 10:08:44 +0100

    feat: create some sort of landing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import LandingPage from "~/pages/Landing.vue";
 import OAuthPage from "~/pages/OAuth.vue";
 import { syncTitle } from "~/router/middleware";
 import AppRoutes from "~/router/modules/app";
@@ -7,6 +8,11 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     ...AppRoutes,
+    {
+      path: "/",
+      name: "landing",
+      component: LandingPage,
+    },
     {
       path: "/oauth",
       name: "oauth",
