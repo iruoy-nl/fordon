37e9dd2d3279d3ba5749c2ffb1a3f959f3a6833a
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-02-01 14:59:37 +0100

    refactor: change small-container to bootstrap

--- src/routes/app/vehicles/+page.svelte
+++ src/routes/app/vehicles/+page.svelte
@@ -5,19 +5,23 @@
 	export let data: PageData;
 </script>
 
-<div class="container-small p-5">
-	<div class="row justify-content-between">
-		<div class="col-auto">
-			<h1>Voertuigen</h1>
-			<p>Een overzicht van al jouw voertuigen.</p>
-		</div>
+<div class="container p-5">
+	<div class="row justify-content-center">
+		<div class="col-8">
+			<div class="row justify-content-between">
+				<div class="col-auto">
+					<h1>Voertuigen</h1>
+					<p>Een overzicht van al jouw voertuigen.</p>
+				</div>
 
-		<div class="col-auto my-auto">
-			<a href="/app/vehicles/add" class="btn btn-primary">Nieuw</a>
+				<div class="col-auto my-auto">
+					<a href="/app/vehicles/add" class="btn btn-primary">Nieuw</a>
+				</div>
+			</div>
+
+			<div class="w-100 my-4" />
+
+			<VehicleList vehicles={data.vehicles} />
 		</div>
 	</div>
-
-	<div class="w-100 my-4" />
-
-	<VehicleList vehicles={data.vehicles} />
 </div>
