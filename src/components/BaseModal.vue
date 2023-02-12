4af2aa283ff4c996cd9f6daeb632ece94b27cdc8
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-02-12 08:34:38 +0100

    feat: add a modal component

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  /**
+   * Whether the model is visible.
+   */
+  modelValue: boolean;
+}>();
+
+defineEmits<{
+  (event: 'update:modelValue', value: boolean): void;
+}>();
+</script>
+
+<template>
+  <template v-if="modelValue">
+    <div class="modal">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </template>
+
+  <template v-if="modelValue">
+    <div class="modal-backdrop show" @click="$emit('update:modelValue', false)"></div>
+  </template>
+</template>
+
+<style scoped lang="scss">
+.modal {
+  display: block;
+  pointer-events: none;
+}
+</style>
