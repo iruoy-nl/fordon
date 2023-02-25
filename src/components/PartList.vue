48dbcec1743db40a27a7fabb9c262029ff010ec6
Youri <89806453+YouriLieverdink@users.noreply.github.com>
2023-02-25 12:06:43 +0100

    refactor: convert parts gallery to a table (#24)

--- src/components/PartList.vue
+++ src/components/PartList.vue
@@ -1,39 +1,129 @@
 <template>
-  <div class="row g-2">
-    <template
-      v-for="part of parts"
-      :key="part.id"
-    >
-      <div class="col-3">
-        <div class="card">
-          <div class="card-body">
-            <a
-              :href="part.url"
-              target="_blank"
-              class="btn btn-link p-0"
-            >
-              {{ part.title }}
-            </a>
-
-            <p class="card-text mt-2">
+  <div class="table-responsive">
+    <table class="table table-bordered table-hover">
+      <thead>
+        <tr>
+          <th class="w-50">
+            Onderdeel
+          </th>
+          <th class="w-50">
+            Voertuig
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <template
+          v-for="part in parts"
+          :key="part.id"
+        >
+          <tr @contextmenu.prevent="openOptionsMenu(part, $event)">
+            <td>
+              <a
+                :href="part.url"
+                target="_blank"
+                class="text-primary"
+              >
+                {{ part.title }}
+              </a>
+            </td>
+            <td>
               <img
                 :src="part.vehicle.photoUrl"
                 style="height: 24px; width: 24px;"
                 class="rounded"
               >
               {{ part.vehicle.model }}
-            </p>
-          </div>
-        </div>
-      </div>
-    </template>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script setup lang="ts">
+import {pipe} from 'fp-ts/lib/function';
+import * as TE from 'fp-ts/lib/TaskEither';
+import {defineAsyncComponent} from 'vue';
+import {closePopUp, openMenu, openModal} from '~/services/pop-up';
+import {deletePartById, updatePartById} from '~/state/part';
 import {Part} from '~/types';
 
 defineProps<{
   parts: Part[];
 }>();
+
+function openOptionsMenu(
+  part: Part,
+  event: MouseEvent
+): void {
+  openMenu({
+    slot: defineAsyncComponent(() => import('~/components/BasePopUpMenu.vue')),
+    position: {x: event.clientX, y: event.clientY},
+    props: {
+      options: [
+        {icon: 'pen', label: 'Wijzigen', onClick: () => editPart(part)},
+        {icon: 'trash', label: 'Verwijderen', onClick: () => deletePart(part)}
+      ]
+    }
+  });
+}
+
+function editPart(
+  part: Part
+): void {
+  openModal({
+    slot: defineAsyncComponent(() => import('~/components/PartForm.vue')),
+    props: {
+      defaultValue: part
+    },
+    emits: {
+      cancel: (): void => {
+        closePopUp();
+      },
+      save: async (data: FormData): Promise<void> => {
+        await pipe(
+          updatePartById(part.id, data),
+          TE.match(
+            (e) => {
+              console.error(e);
+            },
+            () => {
+              closePopUp();
+            }
+          )
+        )();
+      }
+    }});
+}
+
+function deletePart(
+  part: Part
+): void {
+  openModal({
+    slot: defineAsyncComponent(() => import('~/components/PopUpModalConfirm.vue')),
+    props: {
+      title: 'Weet je het zeker?',
+      body: 'Deze actie kan niet ongedaan worden gemaakt.'
+    },
+    emits: {
+      cancel: (): void => {
+        closePopUp();
+      },
+      confirm: async (): Promise<void> => {
+        await pipe(
+          deletePartById(part.id),
+          TE.match(
+            (e) => {
+              console.error(e);
+            },
+            () => {
+              closePopUp();
+            }
+          )
+        )();
+      }
+    }}
+  );
+}
 </script>
